0bdaf0e1fefdc1bfbd4e40535775867c6de90b46
[yuhyun] <[rladbgus781@gmail.com]>
2024-11-05 15:25:08 +0900

    chap2,3,4,5

--- main/java/com/example/firstproject/entity/Article.java
+++ main/java/com/example/firstproject/entity/Article.java
@@ -4,6 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor // 기본 생성자 추가 어노테이션
+@ToString
+
 @Entity // 엔티티 선언
 public class Article {
     @Id // 엔티티의 대푯값 지정
@@ -14,18 +22,4 @@ public class Article {
     @Column
     private String content;
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
